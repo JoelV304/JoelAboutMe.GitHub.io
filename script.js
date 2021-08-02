26dba73ee21341bc088b57fd963db00c6ff55c0a
JoelValerio <88332213+JoelV304@users.noreply.github.com>
2021-08-02 17:34:29 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function hoverGrow(){
+  var grow= document.getElementById('grow').style.width= '20%'
+}
+function hoverShrink(){
+  var shrink= document.getElementById('grow').style.width= "9%"
+}
+function getFormValue(){
+  let fname= document.getElementById('fname').value
+  document.getElementById('nameDisplay').innerHTML= fname
+}
+function delElement(){
+  var element= document.removeChild.getElementById('form')
+}
